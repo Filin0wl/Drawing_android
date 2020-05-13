963b5669b48942b203d2f527a07c4447701dc626
Stacy <nastia-dogf@mail.ru>
2020-05-13 04:05:37 +0300

    load project

---
+++ Canvas_Class.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace Drawing_android
+{
+    [Register("Drawing_android.Canvas_Class")]
+    public class Canvas_Class : LinearLayout
+    {
+        string canvas_text_label;
+        int count = 0;
+        TextView lbl;
+        float x;
+        float y;
+        string sDown;
+        string sMove;
+        string sUp;
+        public Canvas_Class(Context context) : base(context)
+        {
+
+            Initialize(context);
+        }
+
+        public Canvas_Class(Context context, IAttributeSet attrs) : base(context, attrs)
+        {
+            Initialize(context, attrs);
+        }
+
+        private void Initialize(Context context, IAttributeSet attrs = null)
+        {
+            Inflate(context, Resource.Layout.layout_canvas, this);
+            lbl = FindViewById<TextView>(Resource.Id.textView1);
+            InitAttrProperties(context, attrs);
+
+        }
+
+        private void InitAttrProperties(Context context, IAttributeSet attrs)
+        {
+            if (context == null || attrs == null)
+            {
+                return;
+            }
+
+            Android.Content.Res.TypedArray typedArray = context.ObtainStyledAttributes(attrs, Resource.Styleable.CanvasAttr);
+            canvas_text_label = typedArray.GetString(Resource.Styleable.CanvasAttr_text_lable);
+
+            this.Touch += Canvas_Class_Touch;
+        }
+
+        private void Canvas_Class_Touch(object sender, TouchEventArgs e)
+        {
+            x = e.Event.GetX();
+            y = e.Event.GetY();
+
+            switch (e.Event.Action)
+            {
+                case MotionEventActions.Down: // нажатие
+                    sDown = "Down: " + x + "," + y;
+                    sMove = ""; sUp = "";
+                    break;
+                case MotionEventActions.Move: // движение
+                    sMove = "Move: " + x + "," + y;
+                    break;
+                case MotionEventActions.Up: // отпускание
+                case MotionEventActions.Cancel:
+                    sMove = "";
+                    sUp = "Up: " + x + "," + y;
+                    break;
+            }
+            lbl.Text = sDown + "\n" + sMove + "\n" + sUp;
+
+            /* lbl.Text = count.ToString();
+             count++;*/
+        }
+    }
+}
---
+++ MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Views;
+using Java.Security;
+using Java.Lang;
+
+namespace Drawing_android
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity, View.IOnTouchListener
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            /*var view = this.FindViewById(Resource.Id.canvas);
+            view.SetOnTouchListener(this);*/
+        }
+
+        public bool OnTouch(View v, MotionEvent e)
+        {
+            /*switch (e.Action)
+            {
+                case MotionEventActions.Down:
+                    break;
+                case MotionEventActions.Up:
+                    FindViewById<TextView>(Resource.Id.canvas).Text = "tuk";
+                    break;
+            }*/
+            return true;
+        }
+       
+    }
+}
